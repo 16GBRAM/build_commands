0e19c6ea446f51010b267d0b4d1693d6f7efe193
KeleKaPhool <98744626+16GBRAM@users.noreply.github.com>
2025-11-04 10:39:22 +0400

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -16,15 +16,15 @@ rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 
 
-git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b luna device/xiaomi/munch; \
+git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b 15-qpr2 device/xiaomi/munch; \
 
-git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 15-qpr2 vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware -b vic vendor/xiaomi/munch-firmware; \
 
 git clone https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch; \
 
-git clone https://github.com/Lordputin404/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/Lordputin404/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
 
 git clone https://github.com/16GBRAM/android_hardware_dolby.git -b vic hardware/dolby; \
 
