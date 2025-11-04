1b11c127f635f752f2352cb1f27e6f4b819ed4ab
KeleKaPhool <98744626+16GBRAM@users.noreply.github.com>
2025-11-04 12:14:09 +0400

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -20,11 +20,11 @@ git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b 16 devic
 
 git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 16 vendor/xiaomi/munch; \
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware -b vic vendor/xiaomi/munch-firmware; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git -b 16 vendor/xiaomi/munch-firmware; \
 
 git clone https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch; \
 
-git clone https://github.com/Lordputin404/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/16GBRAM/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
 
 git clone https://github.com/16GBRAM/android_hardware_dolby.git -b vic hardware/dolby; \
 
