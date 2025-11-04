1ba8080882890143acb4f45e90e1547e07f4d62e
KeleKaPhool <98744626+16GBRAM@users.noreply.github.com>
2025-11-04 12:54:07 +0400

    Update virus.sh

--- virus.sh
+++ virus.sh
@@ -14,19 +14,25 @@ rm -rf vendor/xiaomi/munch; \
 rm -rf hardware/xiaomi; \
 rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
+rm -rf packages/resources/devicesettings; \
+rm -rf packages/apps/ViPER4AndroidFX; \
 
 
 git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b lunaris device/xiaomi/munch; \
 
-git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 16 vendor/xiaomi/munch; \
+git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 16-exp vendor/xiaomi/munch; \
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware.git -b 16 vendor/xiaomi/munch-firmware; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/SenseiiX/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch; \
+git clone https://github.com/16GBRAM/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch; \
 
-git clone https://github.com/16GBRAM/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
+git clone https://github.com/16GBRAM/android_hardware_xiaomi.git hardware/xiaomi; \
 
-git clone https://github.com/16GBRAM/android_hardware_dolby.git -b vic hardware/dolby; \
+git clone https://github.com/16GBRAM/android_hardware_dolby.git hardware/dolby; \
+
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git -b aosp-16 packages/resources/devicesettings; \
+
+git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera; \
 
