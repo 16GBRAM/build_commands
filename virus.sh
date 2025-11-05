2c879c1d54a2636f5c05dd04888221b714c4f3b3
KeleKaPhool <98744626+16GBRAM@users.noreply.github.com>
2025-11-05 10:11:57 +0400

    voltage

--- virus.sh
+++ virus.sh
@@ -1,10 +1,15 @@
 #! /bin/bash
 
 rm -rf .repo/local_manifests; \
-repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs; \
+repo init -u https://github.com/VoltageOS/manifest.git -b 16 --git-lfs; \
 rm -rf prebuilts/clang/host/linux-x86; \
 /opt/crave/resync.sh; \
 
+# Clone Voltage signing keys and generate them
+git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
+cd vendor/voltage-priv/keys
+chmod +x keys.sh && ./keys.sh
+cd ../../..
 
 rm -rf out/target/product/munch; \
 rm -rf device/xiaomi/munch; \
@@ -15,28 +20,37 @@ rm -rf hardware/xiaomi; \
 rm -rf hardware/dolby; \
 rm -rf vendor/xiaomi/miuicamera; \
 rm -rf packages/resources/devicesettings; \
-rm -rf packages/apps/ViPER4AndroidFX; \
+#rm -rf packages/apps/ViPER4AndroidFX; \
 
 
-git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b lunaris device/xiaomi/munch; \
+git clone https://github.com/16GBRAM/android_device_xiaomi_munch.git -b voltage device/xiaomi/munch; \
 
 git clone https://github.com/16GBRAM/android_vendor_xiaomi_munch.git -b 16-exp vendor/xiaomi/munch; \
 
 git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_munch-firmware -b 16 vendor/xiaomi/munch-firmware; \
 
-git clone https://github.com/16GBRAM/fusionX_sm8250.git -b wip-next kernel/xiaomi/munch; \
+git clone https://github.com/Lordputin404/kernel_xiaomi_munch -b 16-ksu kernel/xiaomi/munch; \
+cd kernel/xiaomi/munch && git submodule init && git submodule update && rm -rf KernelSU-Next/userspace/su && cd ../../..; \
 
-git clone https://github.com/16GBRAM/android_hardware_xiaomi.git hardware/xiaomi; \
+git clone https://github.com/16GBRAM/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi; \
 
-git clone https://github.com/16GBRAM/android_hardware_dolby.git hardware/dolby; \
+git clone https://github.com/16GBRAM/android_hardware_dolby.git -b vic hardware/dolby; \
 
-git clone https://github.com/PocoF3Releases/packages_resources_devicesettings.git -b aosp-16 packages/resources/devicesettings; \
+git clone https://github.com/PocoF3Releases/packages_resources_devicesettings packages/resources/devicesettings; \
 
-git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
+#git clone https://gitlab.com/rik-x777/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX; \
 
-git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera.git -b 16 vendor/xiaomi/miuicamera; \
+git clone https://codeberg.org/munch-devs/android_vendor_xiaomi_miuicamera -b 16 vendor/xiaomi/miuicamera; \
 
 
 . build/envsetup.sh; \
-lunch lineage_munch-bp2a-user
-m lunaris
+
+# Download device_framework_matrix.xml if not present
+if [ ! -f vendor/voltage/config/device_framework_matrix.xml ]; then
+    echo "Downloading device_framework_matrix.xml..."
+    mkdir -p vendor/voltage/config
+    curl -L -o vendor/voltage/config/device_framework_matrix.xml \
+    https://raw.githubusercontent.com/AxionAOSP/android_vendor_lineage/lineage-23.0/config/device_framework_matrix.xml
+fi
+
+brunch munch
